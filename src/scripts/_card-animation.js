79930870ff7bf4eb07cd09cc83224ce4e72d60e2
AndriiManyak <43091048+AndriiManyak@users.noreply.github.com>
2020-11-26 10:13:48 +0200

    Card animation (#53)
    * compressed images for peopleBehind block
    
    * compressed Barcelona images
    
    * compressed Kyiv images
    
    * add another one animation on hover
    
    * change color of animation

---
+++ src/scripts/_card-animation.js
@@ -0,0 +1,32 @@
+const cards = document.querySelectorAll('.news');
+
+const updateStyles = (event, card) => {
+  const { offsetWidth, offsetHeight } = card;
+  const { left, top } = card.getBoundingClientRect();
+  const itemLeft = left + window.scrollX;
+  const itemTop = top + window.scrollY;
+  const size = Math.sqrt((offsetWidth ** 2) + (offsetHeight ** 2));
+
+  card.style.setProperty('--width', `${size}px`);
+  card.style.setProperty('--height', `${size}px`);
+
+  const overlayX = Math.round(event.pageX - itemLeft) - (size / 2);
+  const overlayY = Math.round(event.pageY - itemTop) - (size / 2);
+
+  card.style.setProperty('--top', `${overlayY}px`);
+  card.style.setProperty('--left', `${overlayX}px`);
+};
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', (event) => {
+    card.classList.add('active');
+    updateStyles(event, card);
+  });
+});
+
+cards.forEach(card => {
+  card.addEventListener('mouseleave', (event) => {
+    card.classList.remove('active');
+    updateStyles(event, card);
+  });
+});

b68c8263dc601bc9299c74a7a03efc3bb65be194
Alina <alina90565@gmail.com>
2020-11-28 16:35:51 +0200

    fix threshold for animation and add scrollTop when render page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,8 @@
 import './_select';
 import './_where-to-find';
 import './_card-animation';
-import './_scroll-animation';
+import { addScrollAnimation } from './_scroll-animation';
+
+window.scrollTo(window.pageYOffset, 0);
+
+addScrollAnimation();

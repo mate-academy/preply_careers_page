01aa59267a48c9b53355cde1eb28c6ba888d4831
VoduDay <andriy.maniak@gmail.com>
2020-11-29 13:50:01 +0200

    change hover effect

--- src/scripts/_change-content-animation.js
+++ src/scripts/_change-content-animation.js
@@ -1,16 +1,16 @@
-const cards = document.querySelectorAll('[data-hover="content"]');
+// const cards = document.querySelectorAll('[data-hover="content"]');
 
-cards.forEach(card => {
-  const additional = card.querySelector('.value__additional');
-  const main = card.querySelector('.value__main');
+// cards.forEach(card => {
+//   const additional = card.querySelector('.value__additional');
+//   const main = card.querySelector('.value__main');
 
-  card.addEventListener('mouseenter', () => {
-    additional.classList.add('value__additional--active');
-    main.classList.remove('value__main--active');
-  });
+//   card.addEventListener('mouseenter', () => {
+//     additional.classList.add('value__additional--active');
+//     main.classList.remove('value__main--active');
+//   });
 
-  card.addEventListener('mouseleave', () => {
-    additional.classList.remove('value__additional--active');
-    main.classList.add('value__main--active');
-  });
-});
+//   card.addEventListener('mouseleave', () => {
+//     additional.classList.remove('value__additional--active');
+//     main.classList.add('value__main--active');
+//   });
+// });

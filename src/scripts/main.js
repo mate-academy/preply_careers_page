5093b0b9c05a24c5be064a62c422363fc6693ac1
AndriiManyak <43091048+AndriiManyak@users.noreply.github.com>
2020-11-29 17:58:29 +0200

    Values layout (#91)
    * change values layout
    
    * fixed image position
    
    * change order on smaller screen
    
    * upgrade animation
    
    * change animation direction
    
    * refactored code of animation
    
    * change hover effect
    
    * delete js animatoin file
    
    * fixed main js
    
    * change animation duration
    
    * fixed ordering
    
    * use bootstrap grid layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,6 @@ import './_select';
 import { addListenerForGalleryButtons } from './_button-set-gallery';
 import './_card-animation';
 import { addScrollAnimation } from './_scroll-animation';
-import './_change-content-animation';
 import './_set-year';
 
 document.addEventListener('DOMContentLoaded', () => {

b3afa1f913ee79c0eaaf7c991e7a22968fd34eaf
VoduDay <andriy.maniak@gmail.com>
2020-11-29 14:14:49 +0200

    fixed main js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,5 @@ import './_select';
 import './_where-to-find';
 import './_card-animation';
 import { addScrollAnimation } from './_scroll-animation';
-import './_change-content-animation';
 
 addScrollAnimation();

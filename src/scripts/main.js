c644f4c00e8f7024dc03df284e9b0cfbce44fbcd
Ivan Beletskiy <69988005+IBelet@users.noreply.github.com>
2020-12-01 12:53:25 +0200

    fix (#116)
    Co-authored-by: Viktoriia Serhieieva <62713888+yazheviks@users.noreply.github.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,11 @@ import { startApplication } from './_select';
 import { addListenerForGalleryButtons } from './_button-set-gallery';
 import './_card-animation';
 import { addScrollAnimation } from './_scroll-animation';
-import './_set-year';
+import { setCurrentYear } from './_set-year';
 
 document.addEventListener('DOMContentLoaded', () => {
   addListenerForGalleryButtons();
   addScrollAnimation();
+  setCurrentYear();
   startApplication();
 });

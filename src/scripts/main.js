ebb9804aab95c69d961e1e6e9c17a78a3c210515
Ivan Beletskiy <beletsky02@gmail.com>
2020-12-01 13:03:35 +0200

    add card anim func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 import { startApplication } from './_select';
 import { addListenerForGalleryButtons } from './_button-set-gallery';
-import './_card-animation';
+import { addCardAnimation } from './_card-animation';
 import { addScrollAnimation } from './_scroll-animation';
 import { setCurrentYear } from './_set-year';
 
@@ -10,5 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
   addListenerForGalleryButtons();
   addScrollAnimation();
   setCurrentYear();
+  addCardAnimation();
   startApplication();
 });
